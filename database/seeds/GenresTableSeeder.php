00c479b099026491b0a391a31c050ce7980e13b6
Yuri Calabrez <yuri.calabrez@gmail.com>
2019-08-19 22:13:19 -0300

    Recurso de Gênero

---
+++ database/seeds/GenresTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Genre;
+use Illuminate\Database\Seeder;
+
+class GenresTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Genre::class, 10)->create();
+    }
+}
