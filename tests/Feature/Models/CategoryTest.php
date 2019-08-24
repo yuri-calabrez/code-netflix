f30b57c2f3cea044c514f3e61e9f62bcab1892c7
Yuri Calabrez <yuri.calabrez@gmail.com>
2019-08-24 00:01:45 -0300

    Iniciando testes de itegração

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+   
+    public function testList()
+    {
+        factory(Category::class, 1)->create();
+        $categories = Category::all();
+        $this->assertCount(1, $categories);
+        $categoryKeys = array_keys($categories->first()->getAttributes());
+        $this->assertEqualsCanonicalizing([
+            'id',
+            'name',
+            'description',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'deleted_at'
+        ], $categoryKeys);
+    }
+}
