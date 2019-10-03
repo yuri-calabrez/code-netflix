d190eab42b1b16675915d71e037deefcb87c549a
Yuri Calabrez <yuri.calabrez@gmail.com>
2019-10-02 22:47:09 -0300

    testando relacionamento de categorias e generos

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -20,6 +20,6 @@ class Genre extends Model
 
     public function categories()
     {
-        return $this->belongsToMany(Category::class);
+        return $this->belongsToMany(Category::class)->withTrashed();
     }
 }
---
+++ app/Rules/GenresHasCategoriesRule.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Collection;
+
+class GenresHasCategoriesRule implements Rule
+{
+    /**
+     *
+     * @var array
+     */
+    private $categoriesId;
+
+    /**
+     *  @var array
+     */
+    private $genresId;
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(array $categoriesId)
+    {
+        $this->categoriesId = array_unique($categoriesId);
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $this->genresId = array_unique($value);
+        if (!count($this->categoriesId) || !count($this->genresId)) {
+            return false;
+        }
+
+        $categoriesFound = [];
+        foreach ($this->genresId as $genreId) {
+            $rows = $this->getRows($genreId);
+            if (!$rows->count()) {
+                return false;
+            }
+
+            array_push($categoriesFound, ...$rows->pluck('category_id')->toArray());
+        }
+
+        if (count($categoriesFound) !== count($this->categoriesId)) {
+            return false;
+        }
+        return true;
+    }
+
+    protected function getRows($genreId): Collection
+    {
+        return \DB::table('category_genre')
+            ->where('genre_id', $genreId)
+            ->whereIn('category_id', $this->categoriesId)
+            ->get();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'A genre ID mustbe related at least a category ID';
+    }
+}
