ffb8065a132382035f5038cf74145866c876564e
Yuri Calabrez <yuri.calabrez@gmail.com>
2019-10-26 22:38:54 -0300

    Refatorando testes de genre com resource

---
+++ app/Http/Resources/GenreResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GenreResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'is_active' => $this->is_active,
+            'categories' => CategoryResource::collection($this->categories),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at
+        ];
+    }
+}
