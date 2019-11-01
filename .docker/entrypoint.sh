7bdcdc9e070453ad3a51b9a4503b9ce13e6f436a
Yuri Calabrez <yuri.calabrez@gmail.com>
2019-10-31 22:51:58 -0300

    Criando aplicação React

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
+
+## FRONTEND
+npm config set cache /var/www/.npm-cache --global
+cd /var/www/frontend && npm install && cd ..
+
+##BACKEND
 cd backend
 if [ ! -f ".env" ]; then
     cp .env.example .env
