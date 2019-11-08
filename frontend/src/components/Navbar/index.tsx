0bf816bf9e774bfd4a511e1cdffb2b74f9e5ee3b
Yuri Calabrez <yuri.calabrez@gmail.com>
2019-11-07 23:56:56 -0300

    temas e makestyle

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react'
-import {AppBar, Toolbar} from '@material-ui/core'
+import {AppBar, Toolbar, Typography, Button, makeStyles, Theme} from '@material-ui/core'
+import logo from '../../static/img/logo.png'
+
+const useStyles = makeStyles((theme: Theme) => ({
+    toolbar: {
+        backgroundColor: '#000000'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'center'
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]: {
+            width: 170
+        }
+    }
+}))
 
 export const Navbar: React.FC = () => {
+    const classes = useStyles()
     return (
        <AppBar>
-           <Toolbar>Texto</Toolbar>
+           <Toolbar className={classes.toolbar}>
+               <Typography className={classes.title}>
+                   <img src={logo} alt="Codeflix" className={classes.logo}/>
+               </Typography>
+               <Button color='inherit'>Login</Button>
+           </Toolbar>
        </AppBar>
     )
 }
