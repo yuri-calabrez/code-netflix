3dc1064423afa0d43c5fa91ff8adfbbc063b60a6
Yuri Calabrez <yuri.calabrez@gmail.com>
2019-12-08 19:07:11 -0300

    Sobreescrevendo tipo do body

---
+++ frontend/src/theme.ts
@@ -0,0 +1,24 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#79aec8",
+            contrastText: "#fff"
+        },
+        secondary: {
+            main: "#4db5ab",
+            contrastText: "#fff"
+        },
+        background: {
+            default: "#fafafa"
+        }
+    },
+    overrides: {
+        MUIDataTable: {
+            
+        }
+    }
+})
+
+export default theme
