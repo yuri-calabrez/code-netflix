05c12b13238c07db219eb6d000fad3ff2fc93b2b
Yuri Calabrez <yuri.calabrez@gmail.com>
2020-01-02 23:29:23 -0300

    Montando paginação e ordenação no front end

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -6,9 +6,24 @@ import { BadgeYes, BadgeNo } from '../../components/Badge'
 import { ListResponse, Category } from '../../util/models'
 import DefaultTable, { TableColumn } from '../../components/Table'
 import { useSnackbar } from 'notistack'
+import { IconButton, Link } from '@material-ui/core'
+import EditIcon from '@material-ui/icons/Edit'
+
+interface Pagination {
+    page: number
+    total: number
+    per_page: number
+}
+
+interface Order {
+    sort: string | null
+    dir: string | null
+}
 
 interface SearchState {
     search: string
+    pagination: Pagination
+    order: Order
 }
 
 const columnsDefinition: TableColumn[] = [
@@ -48,7 +63,22 @@ const columnsDefinition: TableColumn[] = [
     {
         name: 'actions',
         label: 'Ações',
-        width: '13%'
+        width: '13%',
+        options: {
+            sort: false,
+            customBodyRender: (value, tableMeta, updateValue) => {
+                /*return (
+                    <IconButton
+                        color='secondary'
+                        component={Link}
+                        to={`/categories/${tableMeta.rowData[0]}/edit`}
+                    >
+                        <EditIcon/>
+                    </IconButton>
+                )*/
+                return 'botão aqui'
+            }
+        }
     }
 ]
 
@@ -58,7 +88,30 @@ const Table = () => {
     const subscribed = React.useRef(true)
     const [data, setData] = React.useState<Category[]>([])
     const [loading, setLoading] = React.useState<boolean>(false)
-    const [searchState, setSearchState] = React.useState<SearchState>({search: ''})
+    const [searchState, setSearchState] = React.useState<SearchState>({
+            search: '',
+            pagination: {
+                page: 1,
+                total: 0,
+                per_page: 10
+            },
+            order: {
+                sort: null,
+                dir: null
+            }
+        })
+
+    const columns = columnsDefinition.map(column => {
+        return column.name === searchState.order.sort
+        ? {
+            ...column,
+            options: {
+                ...column.options,
+                sortDirection: searchState.order.dir as any
+            }
+        }
+        : column
+    })
 
     React.useEffect(() => {
         subscribed.current = true
@@ -66,18 +119,34 @@ const Table = () => {
         return () => {
             subscribed.current = false
         }
-    }, [searchState])
+    }, [
+        searchState.search,
+        searchState.pagination.page,
+        searchState.pagination.per_page,
+        searchState.order
+    ])
 
     async function getData() {
         setLoading(true)
             try {
                 const {data} = await categoryHttp.list<ListResponse<Category>>({
                     queryParams: {
-                        search: searchState.search
+                        search: searchState.search,
+                        page: searchState.pagination.page,
+                        per_page: searchState.pagination.per_page,
+                        sort: searchState.order.sort,
+                        dir: searchState.order.dir
                     }
                 })
                 if (subscribed.current) {
                     setData(data.data)
+                    setSearchState((prevState => ({
+                        ...prevState,
+                        pagination: {
+                            ...prevState.pagination,
+                            total: data.meta.total
+                        }
+                    })))
                 }
             } catch (error) {
                 console.error(error)
@@ -91,13 +160,41 @@ const Table = () => {
 
     return (
         <DefaultTable 
-            columns={columnsDefinition}
+            columns={columns}
             title=""
             data={data}
             loading={loading}
             options={{
+                serverSide: true,
                 searchText: searchState.search,
-                onSearchChange: (value) => setSearchState({search: value})
+                page: searchState.pagination.page - 1,
+                rowsPerPage: searchState.pagination.per_page,
+                count: searchState.pagination.total,
+                onSearchChange: (value) => setSearchState((prevState => ({
+                    ...prevState,
+                    search: value
+                }))),
+                onChangePage:(page) => setSearchState((prevState => ({
+                    ...prevState,
+                    pagination: {
+                        ...prevState.pagination,
+                        page: page + 1
+                    }
+                }))),
+                onChangeRowsPerPage:(perPage) => setSearchState((prevState => ({
+                    ...prevState,
+                    pagination: {
+                        ...prevState.pagination,
+                        per_page: perPage
+                    }
+                }))),
+                onColumnSortChange: (changedColumn: string, direction: string) => setSearchState((prevState => ({
+                    ...prevState,
+                    order: {
+                       sort: changedColumn,
+                        dir: direction.includes('desc') ? 'desc' : 'asc'
+                    }
+                })))
             }}
         />
     )
