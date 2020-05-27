3d3707ce47b0d276f4607d258feeed1a6d5c5cab
Yuri Calabrez <yuri.calabrez@gmail.com>
2020-05-26 23:27:37 -0300

    Criando Observers para Genre e CastMember

--- backend/app/Observers/CategoryObserver.php
+++ backend/app/Observers/CategoryObserver.php
@@ -27,7 +27,8 @@ class CategoryObserver
      */
     public function updated(Category $category)
     {
-        //
+        $message = new Message($category->toJson());
+        \Amqp::publish('model.category.updated', $message);
     }
 
     /**
@@ -38,7 +39,8 @@ class CategoryObserver
      */
     public function deleted(Category $category)
     {
-        //
+        $message = new Message(json_encode(['id' => $category->id]));
+        \Amqp::publish('model.category.deleted', $message);
     }
 
     /**
--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Category;
-use App\Observers\CategoryObserver;
+use App\Models\{CastMember, Category, Genre};
+use App\Observers\{CastMemberObserver, CategoryObserver, GenreObserver};
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Category::observe(CategoryObserver::class);
+        Genre::observe(GenreObserver::class);
+        CastMember::observe(CastMemberObserver::class);
     }
 }
